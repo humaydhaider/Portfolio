36c74805bb102f596a7d3020fa1bdfbdf087e35a
humaydhaider <102238663+humaydhaider@users.noreply.github.com>
2022-03-23 19:38:34 +0500

    Update Covid query.sql

--- Covid query.sql
+++ Covid query.sql
@@ -23,7 +23,7 @@ From covid_db
 Group by Country_reg
 Order by 3 DESC, 4 DESC;
 
---[India leads the recovery rates in the world]
+--#India leads the recovery rates in the world
 
 ---Which countries had the most recoveries by August 2021? Listing the top 20
 
@@ -31,15 +31,26 @@ Select	country_reg AS Countries,
 		Months,
 		Total_Recoveries
 from
-(Select country_reg,
-		DATENAME(month,date) AS Months,
-		MAX(recovered) As Total_Recoveries,
-		RANK() OVER (Partition by DATENAME(month,date) Order by MAX(recovered) DESC) AS 'Rank1'
-From covid_db
+  (Select 
+ 	country_reg,
+	DATENAME(month,date) AS Months,
+	MAX(recovered) As Total_Recoveries,
+	RANK() OVER (Partition by DATENAME(month,date) Order by MAX(recovered) DESC) AS 'Rank1'
+From 
+ covid_db
+ 
 Where date = '2021-08-01'
-Group by Country_Reg, DATENAME(month,date))
+ 
+Group by 
+ 	Country_Reg, 
+ 	DATENAME(month,date))
+	
 A Where Rank1 Between 1 AND 20
-Group by Country_Reg, Months, Total_Recoveries;
+
+Group by 
+	Country_Reg,
+	Months, 
+	Total_Recoveries;
 
 
 ---- Top 25 countries with highest vaccinations
@@ -56,8 +67,9 @@ Select	Vacc_Year,
 		Countries, 
 		Vaccinations, 
 		Popular_Vaccines 
-	from
-	(Select DATENAME(Year, date) AS Vacc_Year,
+from
+	(Select 
+	 	DATENAME(Year, date) AS Vacc_Year,
 		country as Countries,
 		MAX(total_vaccinations) AS Vaccinations,
 		vaccines as Popular_Vaccines,
@@ -68,24 +80,22 @@ Group by country, vaccines, DATENAME(Year,date))
 
 A Where VaccRank BETWEEN 1 and 25
 
-Group by	Vacc_Year,
-			Countries, 
-			Vaccinations, 
-			Popular_Vaccines
+Group by
+	Vacc_Year,
+	Countries, 
+	Vaccinations, 
+	Popular_Vaccines
 
 Order by 1 ASC;
 
-----------
-Select * from dbo.covid_db;
-Select * from dbo.country_vaccinations$;
 
 ----- Countries currently struggling with Covid-19 as of March, 2022
 
 Select 
-		cov.Country_reg, 
-		cov.Province, 
-		MAX(vac.people_vaccinated) AS 'People Vaccinated',
-		MAX(cov.active) AS 'Active Cases'
+	cov.Country_reg, 
+	cov.Province, 
+	MAX(vac.people_vaccinated) AS 'People Vaccinated',
+	MAX(cov.active) AS 'Active Cases'
 From 
 
 	covid_db cov
@@ -104,37 +114,41 @@ Order by 4 DESC;
 
 --#Countries are still struggling with the pandemic, even after successful vaccination campaigns.
 
-Select * from dbo.covgdp;
-Select * from dbo.covid_db;
 
----Looking at Recovered patients with respect to GDP Per capita.
+--- Looking at Recovered patients with respect to GDP Per capita.
 
 Select 
-		cov.Country_reg, 
-		MAX(cov.recovered) AS 'Recoveries',
-		ROUND(gd.gdp_per_capita,2) AS 'GDP Per Capita'
+	cov.Country_reg, 
+	MAX(cov.recovered) AS 'Recoveries',
+	ROUND(gd.gdp_per_capita,2) AS 'GDP Per Capita'
 
-	From
-
-covid_db cov
+From
+	covid_db cov
+	
 JOIN
 covgdp gd ON cov.Country_Reg = gd.location
 AND cov.date = gd.date
-Group by	cov.country_reg, 
-			gd.gdp_per_capita
+
+Group by
+	cov.country_reg, 
+	gd.gdp_per_capita
 
 Order by 2 DESC;
 
 --# There seems to be no definitive, identifiable correlation between GDP Per Capita and Recoveries made.
 
+
 ---- Comparing male and female smoker percentage with the number of deaths.
 
-Select	cov.country_reg, 
-		MAX(cov.deaths) AS Deaths,
-		MAX(gd.male_smokers) AS 'Male Smokers %',
-		MAX(gd.female_smokers) AS 'Female Smokers %'
-	From
-covid_db cov
+Select	
+	cov.country_reg, 
+	MAX(cov.deaths) AS Deaths,
+	MAX(gd.male_smokers) AS 'Male Smokers %',
+	MAX(gd.female_smokers) AS 'Female Smokers %'
+	
+From
+	covid_db cov
+	
 JOIN
 covgdp gd ON cov.Country_Reg = gd.location
 AND cov.date = gd.date
@@ -145,13 +159,14 @@ Order by 2 DESC;
 
 -- Looking at the Case and Death count per Continent
 
-Select	gd.continent AS Continent, 
-		MAX(cov.confirmed_cases) 'Confirmed cases', 
-		MAX(cov.deaths) 'Deaths'
+Select	
+	gd.continent AS Continent, 
+	MAX(cov.confirmed_cases) 'Confirmed cases', 
+	MAX(cov.deaths) 'Deaths'
 
-	From
-
-covgdp gd
+From
+	covgdp gd
+	
 INNER JOIN
 covid_db cov ON gd.location = cov.Country_Reg
 AND gd.date = cov.date
@@ -160,9 +175,8 @@ Order by 3 DESC;
 
 --#North America dealt the best as their deaths are lower in proportion in comparison with their total cases
 
-
-
-
+/*
+	End of SQL Query \
 
 ---Notes / Comments
 
@@ -171,6 +185,7 @@ Order by 3 DESC;
 --Exec sp_rename 'data_2$', 'covgdp';
 
 ---Select * from dbo.covid_db;
+---Select * from dbo.country_vaccinations$;
 
 --Use PortfolioProject;
 
@@ -180,13 +195,3 @@ Order by 3 DESC;
 --Importing covid_2022 data file to compare GDP and smokers count.
 
 --Select * from dbo.country_vaccinations$;
-
-
-
-
-
-
-
-
-
-
